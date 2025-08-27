13b45bf7217cc0b1f6a023213fbbfb9c9c49d725
jamal ahmed <jamalahmed1122teamx@gmail.com>
2025-08-27 17:39:35 +0500

    updated something

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Added a new line
+          Edit <code>src/App.js</code> and save to reload. Added a new line this is very nice now i will get conflicts
         </p>
         <a
           className="App-link"
