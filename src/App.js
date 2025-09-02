4ab0d5087001842169a75f8e7d590d553ab56900
jamal ahmed <jamalahmed1122teamx@gmail.com>
2025-09-02 19:59:37 +0500

    commit in new feat 01

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        learn react  as
+        learn react as adas dasd asdas das asd as 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        {/* learn react */} I am nice I am here now i am here in the feat branch
+        {/* learn react */} I am nice I am here now i am here in the feat branch asdasdsad sdasdasgd jkasb djk asd asdas 
         </a>
       </header>
     </div>
